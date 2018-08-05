c6e7dddafb268df7f7e0237fc36c6b71b9558df9
Vamsi Tallapudi <vamsi@coderefer.com>
2018-08-05 12:13:02 +0530

    Revert "Chapter 1 - 1.2 Check Permutation finalized answer

--- src/chapter1/CheckPermutation.kt
+++ src/chapter1/CheckPermutation.kt
@@ -3,11 +3,18 @@ package chapter1
 import java.util.*
 
 fun main(args: Array<String>) {
-    sort("vamsi")
+    val str1 = readLine()!!
+    val str2 = readLine()!!
+    when {
+        str1.length!=str2.length -> print("Two strings are not permutations")
+        sort(str1) == sort(str2) -> println("Valid permutations")
+        else -> println("Permutations not valid")
+    }
+
 }
 
-fun sort(str:String) {
+fun sort(str:String) :String {
     val y = str.toCharArray()
     Arrays.sort(y)
-    println(y)
+    return String(y)
 }
